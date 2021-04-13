0aedad5010f42d573a1e9a96264a3c6d22330df7
Rick Slingerland <74539046+Kotorem-sama@users.noreply.github.com>
2021-04-13 22:07:00 +0200

    Finally JSON
    I changed the pickle to json. It is a bit slower for now, but i will try to find a way to make it faster, but i did this mainly for the conversion to c#

--- balls.py
+++ balls.py
@@ -1,16 +1,35 @@
 import json
 import os
 import pickle
+import sys
+import timeit
 
 directory = os.path.dirname(os.path.realpath(__file__)) + '\\'
+def test1():
+    with open(directory + 'Tests/' + 'Listofanimes.pkl', "rb") as L:
+        listofanimes = pickle.load(L)
 
-open_file = open(directory + 'Tests/' + 'Listofanimes.pkl', "rb")
-listofanimes = pickle.load(open_file)
-open_file.close()
+# # split listofanimes in seperate lists and converting them to json string
+# list1 = listofanimes[0:10000]
+# list1 = json.dumps(list1)
+# list2 = listofanimes[10000:20000]
+# list2 = json.dumps(list2)
+# list3 = listofanimes[20000:-1]
+# list3.append(listofanimes[-1])
+# list3 = json.dumps(list3)
 
-# with open(directory + "JSON\\Listofanimes.json", "w") as f:
-#     data = json.load(f)
-# print(data)
+# with open(directory + "JSON\\Listofanimes1.json", "w") as f:
+#     json.dump(list1, f)
 
-with open(directory + "JSON\\Listofanimes.json", "w") as json_file:
-    json.dump(listofanimes, json_file)
+# with open(directory + "JSON\\Listofanimes2.json", "w") as f:
+#     json.dump(list2, f)
+
+# with open(directory + "JSON\\Listofanimes3.json", "w") as f:
+#     json.dump(list3, f)
+
+def test2():
+    check = ["Listofanimes1.json", "Listofanimes2.json", "Listofanimes3.json"]
+    fulllist1234 = []
+    for i in check:
+        with open(directory + "JSON\\" + i, "r") as f:
+            fulllist1234 += json.loads(json.load(f))
